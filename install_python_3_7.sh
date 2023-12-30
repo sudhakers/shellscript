258e7b7389786cb6a5c53901e9db06e1571ca4e5
SudhakerS <sudhaker.shabada@gmail.com>
2023-12-30 09:37:17 +0530

    .\demo.sh

---
+++ install_python_3_7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.7
+python3.7 --version
